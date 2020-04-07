0239a6643517081f8239808bb9e0ca223db5bb92
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-07 15:50:30 -0400

    Created a axios get request that successfully pulled data down from the api

--- user-cards/src/App.js
+++ user-cards/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends React.Component {
@@ -9,12 +10,25 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    axios.get("https://api.github.com/users/Tusk617")
+    .then(response => {
+      console.log(response.data);
+      this.setState({
+        members: response.data.avatar_url
+      })
+    })
+  }
+
   render() {
   return (
     <div className="App">
       <header className="Page-header">
         <h1>Meet The Team!</h1>
       </header>
+      <div className="memberCards">
+
+      </div>
     </div>
   );
 }
